e291f2c6ef046f374ac4f06f4619cfd2ff2f708e
Юлия Денисова <filippenkojulia@MacBook-Air-Ulia.local>
2023-04-27 13:31:59 +0500

    укороченный вариант

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,28 @@
-import { useEffect, useRef, useState } from 'react';
+import { UIEvent, useRef, useState } from 'react';
 import { data } from './mockData';
 import './App.css'
 
 function App() {
-  const divRef = useRef<HTMLDivElement | null>(null)
+  const uListRef = useRef<HTMLUListElement | null>(null)
   const rowHeight: number = 60
   const visibleRows: number = 4
   const [firstVisibleStroke, setFirstVisibleStroke] = useState<number>(0);
-  const topDivHeight: number = rowHeight * firstVisibleStroke
-  const bottomDivHeight: number = rowHeight * (data.length - (firstVisibleStroke + visibleRows))
 
-  const onScroll = (e: Event): void => {
+  const update = (e: UIEvent<HTMLDivElement>): void => {
     const target = e.target as HTMLDivElement;    
-    setFirstVisibleStroke(Math.floor(target.scrollTop / rowHeight))
-  }
-
-  useEffect(() => {
-    divRef?.current?.addEventListener('scroll', onScroll);
-    return () => divRef?.current?.removeEventListener("scroll", onScroll);
-    //eslint-disable-next-line
-  }, [])
+    setFirstVisibleStroke(Math.floor(target.scrollTop / rowHeight));
+  };
 
   return (
     <div className='wrapper'>
-      <div ref={divRef} style={{ height: rowHeight * visibleRows }}>
-        <div style={{ height: topDivHeight }}></div>
-        <ul>
+      <div style={{ height: rowHeight * visibleRows }} onScroll={update}>
+        <ul ref={uListRef} style={{ height: rowHeight * data.length }}>
           {data.slice(firstVisibleStroke, firstVisibleStroke + visibleRows + 1).map((str, index) =>
-            <li key={firstVisibleStroke + index} style={{ height: rowHeight }}>
+            <li key={firstVisibleStroke + index} style={{ height: rowHeight, top: rowHeight * firstVisibleStroke }}>
               <span>{str}</span>
             </li>
           )}
         </ul>
-        <div style={{ height: bottomDivHeight }}></div>
       </div>
     </div>
   );
