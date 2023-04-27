87ffa32a41bb208b413ab41d895273a0fae504d9
Юлия Денисова <filippenkojulia@MacBook-Air-Ulia.local>
2023-04-27 13:38:16 +0500

    убрал юз-эффект, повесил обработчик напрямую

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { UIEvent, useRef, useState } from 'react';
 import { data } from './mockData';
 import './App.css'
 
@@ -10,20 +10,14 @@ function App() {
   const topDivHeight: number = rowHeight * firstVisibleStroke
   const bottomDivHeight: number = rowHeight * (data.length - (firstVisibleStroke + visibleRows))
 
-  const onScroll = (e: Event): void => {
+  const update = (e: UIEvent<HTMLDivElement>): void => {
     const target = e.target as HTMLDivElement;    
     setFirstVisibleStroke(Math.floor(target.scrollTop / rowHeight))
   }
 
-  useEffect(() => {
-    divRef?.current?.addEventListener('scroll', onScroll);
-    return () => divRef?.current?.removeEventListener("scroll", onScroll);
-    //eslint-disable-next-line
-  }, [])
-
   return (
     <div className='wrapper'>
-      <div ref={divRef} style={{ height: rowHeight * visibleRows }}>
+      <div ref={divRef} style={{ height: rowHeight * visibleRows }} onScroll={update}>
         <div style={{ height: topDivHeight }}></div>
         <ul>
           {data.slice(firstVisibleStroke, firstVisibleStroke + visibleRows + 1).map((str, index) =>
